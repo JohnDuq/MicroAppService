37b54f4669066d2126f176d4c67535055945295f
jhdramo <jhdramo@bancolombia.com.co>
2019-12-13 15:40:47 -0500

    Ajuste Para Heroku

--- micro-app-view/src/server.js
+++ micro-app-view/src/server.js
@@ -3,9 +3,9 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(__dirname+'/dist/micro-app-view'));
+app.use(express.static(__dirname+'/micro-app-view/dist/micro-app-view'));
 app.get('/',function(req,res){
-    res.sendFile(path.join(__dirname+'/dist/micro-app-view/index.html'));
+    res.sendFile(path.join(__dirname+'/micro-app-view/dist/micro-app-view/index.html'));
 });
 
 app.listen(process.env.PORT || 8080);
